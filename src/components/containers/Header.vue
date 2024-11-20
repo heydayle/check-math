934fddc4697c7520969a39046b579bc9d9faaa74
heyday <hungthinh.ckc@gmail.com>
2024-11-20 08:54:16 +0700

    chore: logo

--- src/components/containers/Header.vue
+++ src/components/containers/Header.vue
@@ -4,20 +4,31 @@ import { useToggle, useDark, useLocalStorage } from '@vueuse/core'
 
 const isDark = useLocalStorage('dark', false)
 function toggleDarkMode() {
-  document.documentElement.classList.toggle('p-dark');
+  document.documentElement.classList.toggle('p-dark')
 }
 </script>
 <template>
-  <div class="z-100 bg-background/75 backdrop-blur border-gray-200 dark:border-gray-800 -mb-px sticky top-0 z-50 lg:mb-0 lg:border-0 p-2 flex justify-between">
-    <Button as="router-link" variant="text" to="/" size="small" class="font-bold pt-2" >CHECK.MATH</Button>
+  <div
+    class="z-100 bg-background/75 backdrop-blur border-gray-200 dark:border-gray-800 -mb-px sticky top-0 z-50 lg:mb-0 lg:border-0 p-2 flex justify-between"
+  >
+    <Button as="router-link" variant="text" to="/" size="small" class="font-bold pt-2"
+      >CHECK.MATH</Button
+    >
     <div>
       <ul class="flex items-center space-x-4">
         <li>
           <Button rounded outlined size="small">v0.0.1</Button>
         </li>
         <li>
-          <Button rounded outlined size="small" as="a" href="https://github.com/heydayle" target="_blank">
-            Github <i class="pi pi-arrow-up-right !text-xs"/>
+          <Button
+            rounded
+            outlined
+            size="small"
+            as="a"
+            href="https://github.com/heydayle"
+            target="_blank"
+          >
+            Github <i class="pi pi-arrow-up-right !text-xs" />
           </Button>
         </li>
         <li>
@@ -31,5 +42,4 @@ function toggleDarkMode() {
     </div>
   </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
